60c8d6f3aaafd473fe9ce7e1d2e588acddf75d53
NguyenChiThanh2 <111903264+NguyenChiThanh2@users.noreply.github.com>
2025-06-12 23:52:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,7 @@
     <?php
 
         include_once("trangchu.php");
-        // if(isset($_GET["dangnhap"])){
-        //     include_once("view/dangnhap.php");
-        // }
-        // elseif(isset($_GET["chitietsanpham"])){
-        //     include_once("view/chitietsanpham/index.php");
-        // }elseif(isset($_GET["dangky"])){
-        //     include_once("view/dangky/index.php");
-        // }else{
-        //     include_once("view/SanPham/index.php");
-        // }        
-        // include_once("view/giaodien/footer.php");
-        // ob_end_flush();
+        
     ?>
 
 </html>
